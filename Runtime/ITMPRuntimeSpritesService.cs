19c775027ea9978d46a3365ea8d5133fab8986a6
Nickolai Savelev <nickolai.savelev@gmail.com>
2024-01-24 14:24:51 +0300

    voffset addded to interface

--- Runtime/ITMPRuntimeSpritesService.cs
+++ Runtime/ITMPRuntimeSpritesService.cs
@@ -7,8 +7,8 @@ namespace TMP_SpriteExtension.Runtime
     public interface ITMPRuntimeSpritesService
     {
         void Init();
-        void RegisterSprites(IEnumerable<Sprite> sprites);
-        void RegisterAtlas(SpriteAtlas atlas);
-        void RegisterAtlases(IEnumerable<SpriteAtlas> atlases);
+        void RegisterSprites(IEnumerable<Sprite> sprites, float vOffset);
+        void RegisterAtlas(SpriteAtlas atlas, float vOffset);
+        void RegisterAtlases(IEnumerable<SpriteAtlas> atlases, float vOffset);
     }
 }
